5403d5abf61257fb45e2f69aec2a2d2539e36b60
Jorge Manrubia <jorge.manrubia@gmail.com>
2017-05-06 17:49:30 +0200

    Add slider and label

--- MainWindowController.swift
+++ MainWindowController.swift
@@ -1,5 +1,11 @@
 import Cocoa
 
 class MainWindowController: NSWindowController{
+    override var windowNibName: String {
+        return "MainWindowController"
+    }
     
+    @IBAction func adjustRed(_ sender: NSSlider) {
+        print("R slider value is \(sender.integerValue)")
+    }
 }
